1f042e62a3851466531bd207acf724d14bb1db32
thuodev <samcreations104@gmail.com>
2025-08-16 15:09:33 +0300

    Add files via upload

---
+++ week 2 assignment.py
@@ -0,0 +1,9 @@
+my_list = []
+my_list.extend([10, 20, 30, 40])
+my_list.insert(1, 15)
+my_list.extend([50, 60, 70])
+my_list.pop()
+my_list.sort()
+
+print("Updated List:", my_list)
+print("Index of 30:", my_list.index(30))
